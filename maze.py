f18894bc9c52b872cc9df6797951a96ffcf7d44f
chointer <chointer04@gmail.com>
2024-06-26 18:56:38 +0900

    Feat: MazeEnv observation, reset, generate_maze 구현
    1. 미로 생성 설정 수정
    에피소드마다 다양한 크기의 미로를 생성할 수 있도록 만들기 위해, `__init__`의 입력으로 미로 생성 크기 범위를 받아 저장하도록 설정했다.
    
    2. observation; `_get_obs` 구현
    observation을 반환하는 함수 구현
    
    3. `reset` 구현
    미로 크기 결정, 미로 생성, agent와 target의 위치 초기화
    렌더링 설정은 렌더링 함수 구현 후 적용할 예정
    
    4. `generate_maze` 구현
    maze_maker.py의 미로 생성 함수(`MazeMaker.generate`)를 gymnasium 환경에 넣었다. (이후, 미로 데이터 타입 변경도 환경에 추가하는 것이 좋지않을까)
     - 난수 생성 함수를 모두 gymnasium Env에서 제공하는 `self.np_random`으로 변경
     - 기존에 dir2idx로 방향(string)과 방향 인덱스를 연결지어 사용했는데, 바로 방향 인덱스를 사용하도록 수정
    
    Related to: #2

--- maze.py
+++ maze.py
@@ -4,29 +4,29 @@ import pygame
 import gymnasium as gym
 from gymnasium import spaces
 
-from maze_maker import MazeMaker
+#from maze_maker import MazeMaker
 
 
 class MazeEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}     # * NOTE. 나중에 언제 쓰이는지 체크하기
 
-    def __init__(self, render_mode=None, maze_width=10, maze_height=10):
-        self.maze_width = maze_width
-        self.maze_width = maze_height
+    def __init__(self, render_mode=None, height_range=[5, 10], width_range=[5, 10]):
+        self.height_range = height_range
+        self.width_range = width_range
         self.window_size = 720          # Pygame window size
 
-        # Create a placeholder? for Observation Space - locations of the agent and the goal
+        # Create a placeholder? for Observation Space - locations of the agent and the target
         self.observation_space = spaces.Dict(
             {
                 "agent": spaces.Box(
                     np.array([0, 0]), 
-                    np.array([self.maze_height - 1, self.maze_width - 1]), 
+                    np.array([self.height_range[1] - 1, self.width_range[1] - 1]), 
                     shape=(2,),
                     dtype=int
                     ),
-                "goal": spaces.Box(
+                "target": spaces.Box(
                     np.array([0, 0]), 
-                    np.array([self.maze_height - 1, self.maze_width - 1]), 
+                    np.array([self.height_range[1] - 1, self.width_range[1] - 1]), 
                     shape=(2,),
                     dtype=int
                     )
@@ -50,15 +50,86 @@ class MazeEnv(gym.Env):
         self.window = None      # a reference(참조) to the window that we draw to
         self.clock = None       # clock for correct framerate
 
-"""
+
     def _get_obs(self):
+        return {"maze": self.maze, "agent": self._agent_location, "target": self._target_location}
 
 
+    def reset(self, seed=None, nr_ratio=0.75):
+        super().reset(seed=seed)
+
+        # initialize maze size       
+        self.maze_height = self.np_random.integers(self.height_range[0], self.height_range[1])
+        self.maze_width = self.np_random.integers(self.width_range[0], self.width_range[1])
+
+        # make maze
+        self.maze = self.generate_maze(self.maze_height, self.maze_width, nr_ratio=nr_ratio)
+        
+        # initialize object locations
+        self._agent_location = self.np_random.integers([self.maze_height, self.maze_width], size=2, dtype=int)
+        self._target_location = self._agent_location
+        while np.array_equal(self._agent_location, self._target_location):
+            self._target_location = self.np_random.integers([self.maze_height, self.maze_width], size=2, dtype=int)
+
+        # RETURNS
+        observation = self._get_obs()
+        
+        #if self.render_mode == "human":
+        #    self._render_frame()
+
+        return observation
+        
+
+    def generate_maze(self, height, width, nr_ratio=0.75):
+        wall_c = np.ones((height, width, 4), dtype=bool)
+        cell_visited = np.zeros((height, width), dtype=bool)
+        cell_stack = []
+
+        # [step 1] Select initial cell and stack.
+        cell_current = self.np_random.integers([self.maze_height, self.maze_width], size=2, dtype=int) # modified: np.random.randint -> np_random.integers
+        cell_stack.append(cell_current)
+        cell_visited[cell_current[0], cell_current[1]] = True
+        
+        # [step 2] Remove wall
+        while cell_stack:
+            # [step 2.1] Select cell_current; Newest vs. Random
+            if self.np_random.random() <= nr_ratio:    # modified: np.random.rand -> self.np_random.random
+                cell_current_idx = -1       # Newest; Recursive Backtracking
+            else:
+                cell_current_idx = self.np_random.integers(len(cell_stack))       # Random; Prim's Algorithm, modified: np.random.randint -> self.np_random.integers
+            cell_current = cell_stack[cell_current_idx]
+
+            # [step 2.2] Check adjacent cells & remove wall
+            #for direction in np.random.permutation(["Up", "Down", "Left", "Right"]):
+            for direction_idx in self.np_random.permutation(4):        # modified: np.random.permutation(["Up", "Down", "Left", "Right"]) -> self.np_random.permutation(4)
+                direction = self._action_to_direction[direction_idx]   # direction_idx {0, 1, 2, 3} = direction {up, down, left, right}
+                cell_next = cell_current + direction    #self.dir2move[direction]
+                # [2.2.a] 방문하지 않은 cell이면, stack cell_next, visit check, remove wall
+                if cell_next[0] >= 0 and cell_next[0] < height and cell_next[1] >= 0 and cell_next[1] < width and not cell_visited[cell_next[0], cell_next[1]]:
+                    cell_stack.append(cell_next)
+                    cell_visited[cell_next[0], cell_next[1]] = True
+                    wall_c[cell_current[0], cell_current[1], direction_idx] = False
+                    direction_idx_opposite = 2*(direction_idx//2) + (direction_idx + 1)%2
+                    wall_c[cell_next[0], cell_next[1], direction_idx_opposite] = False
+                    break
+                # [2.2.b] (no code) 이미 방문된 셀이면, 다음 방향 순서로 반복
+
+            # [2.2.c] 더 이상 방문할 셀이 없으면, stack에서 현재 cell 제거. break 없이 for 반복을 완료한 상태이므로 else로 진입하게 된다.
+            else:
+                del cell_stack[cell_current_idx]
+
+        return wall_c
 
 
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+        
 
-
-
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pass
+"""
 
 def render(wall_c, w=10, h=10, screen_w=720, screen_h=720):
     pygame.init()
@@ -97,3 +168,12 @@ maze_maker = MazeMaker()
 w, h, wall_c = maze_maker.generate(10, 10)
 
 """
+
+env = MazeEnv(height_range=[2, 5], width_range=[4, 5])
+
+Hs = np.zeros(12)
+Ws = np.zeros(12)
+
+for i in range(1):
+    env.reset()
+    
